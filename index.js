aa08f0356afba13b89415de98018f4d9c384860c
Mbuyu Makayi <mbmakayi@gmail.com>
2020-01-26 02:02:32 +0200

    graphql tests

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ app.use('/graphql', graphqlHTTP({
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+
+module.exports=app;
---
+++ tests/graphql.js
@@ -0,0 +1,22 @@
+const {expect} = require('chai');
+
+const server = require('../index');
+const request = require('supertest');
+
+describe('GraphQL', () => {
+    it('Returns user with id = 10', (done) => {
+        request(server).get('/graphql/')
+        .send({ query: '{  posts { description} } '})
+        .expect(200)
+        .end((err,res) => {
+            // res will contain array with one user
+            if (err) return done(err);
+            expect(Array.isArray(res.body.posts))
+            expect(res.body.data).to.have.property('posts')
+            expect(res.body.data.posts[0]).to.have.property('description')
+            done();
+        })
+    })
+
+    
+});
